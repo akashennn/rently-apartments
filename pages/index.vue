cced99f34f3fe3e56d641c4b07d301bf6026cd59
Akash Perera <akash.perera@chope.co>
2023-01-03 15:21:30 +0530

    add onCardClick

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,34 @@
 <template>
-    <p>Apartments Listing Page</p>
+    <div class="grid m-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
+        <div v-for="(apartment, index) in apartments" :key="apartment.id" class="card bg-base-100 shadow-xl"
+            @click="onCardClick(apartment.id)">
+            <figure>
+                <img :src="apartment.image" :alt="apartment.title" class="object-cover h-[300px] w-full" />
+            </figure>
+
+            <div class="card-body">
+                <h2 class="card-title">
+                    {{ apartment.title }}
+                    <div v-if="index === 0" class="badge badge-secondary">NEW</div>
+                </h2>
+
+                <p class="mb-2">{{ apartment.address }}</p>
+
+                <div class="card-actions justify-end">
+                    <div class="badge badge-outline">Floor {{ apartment.floor }}</div>
+                    <div class="badge badge-outline">Unit {{ apartment.doorNumber }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-export default {
+<script setup>
+//  get all the apartments data
+const { data: apartments } = await useFetch("http://localhost:3004/apartments");
 
+// navigate to apartment details page
+const onCardClick = async (id) => {
+    await navigateTo(`/apartments/${id}`)
 }
 </script>
-
-<style>
-
-</style>
