3477f275c4ec5a6bd679bf8837bcd2a8d7a4b673
Akash Perera <akash.perera@chope.co>
2023-01-03 21:06:36 +0530

    fix mobile styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
-    <div class="grid m-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
-        <div v-for="(apartment, index) in apartments" :key="apartment.id" class="card bg-base-100 shadow-xl"
-            @click="onCardClick(apartment.id)">
-            <figure>
+    <div class="grid grid-cols-1 md:grid-cols-2 md:gap-2 md:m-2 lg:grid-cols-3 lg:gap-4 lg:m-4">
+        <div v-for="(apartment, index) in apartments" :key="apartment.id"
+            class="card bg-base-100 shadow-xl rounded-none" @click="onCardClick(apartment.id)">
+            <figure class="rounded-none">
                 <img :src="apartment.image" :alt="apartment.title" class="object-cover h-[300px] w-full" />
             </figure>
 
             <div class="card-body">
                 <h2 class="card-title">
                     {{ apartment.title }}
+
                     <div v-if="index === 0" class="badge badge-secondary">NEW</div>
                 </h2>
 
