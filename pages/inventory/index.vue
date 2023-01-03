39c30f700a954327f3ff9c5659eff7df2a2e3c77
Akash Perera <akash.perera@chope.co>
2023-01-03 23:02:23 +0530

    fix route

--- pages/inventory/index.vue
+++ pages/inventory/index.vue
@@ -2,7 +2,7 @@
     <div
         class="grid grid-cols-1 md:grid-cols-2 md:gap-2 md:m-2 lg:grid-cols-3 lg:m-0 mb-[64px] md:mb-[76px] lg:mb-[12px]">
         <div v-for="item in inventory" :key="item.id" class="card bg-base-100 shadow-xl rounded-none"
-            @click="navigateTo(`/apartments/${item.id}`)">
+            @click="navigateTo(`/inventory/${item.id}`)">
             <img :src="item.image" :alt="item.name" class="object-cover h-[300px] w-full" />
 
             <div class="card-body">
